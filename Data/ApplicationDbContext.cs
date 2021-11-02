a21e2f0b6a45f5f09369b3201f2d840962d18e9b
acieffe <acieffe@gmail.com>
2021-11-01 21:13:41 -0400

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rolodex.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rolodex.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+    }
+}
